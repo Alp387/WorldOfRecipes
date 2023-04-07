b3d8ddbda8cb58849f66233b9b4423a6497bb6ea
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-04-08 00:31:58 +0200

    GetMapping getAllRecipes(), Repository contains hardcoded 3 Recipes

--- backend/src/main/java/de/worldofrecipes/backend/RecipeRepository.java
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeRepository.java
@@ -1,7 +1,22 @@
 package de.worldofrecipes.backend;
 
+import de.worldofrecipes.backend.model.Recipe;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class RecipeRepository {
+    private Map<String,Recipe> recipeMap;
+    public RecipeRepository(){
+        recipeMap.put("1",new Recipe("1","Cacik"));
+        recipeMap.put("2",new Recipe("2","Linsensuppe"));
+        recipeMap.put("3",new Recipe("3","Milchreis"));
+    }
+
+    public List<Recipe> getAllRecipes() {
+        return new ArrayList<>(recipeMap.values());
+    }
 }
--- backend/src/main/java/de/worldofrecipes/backend/RecipeService.java
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeService.java
@@ -1,9 +1,23 @@
 package de.worldofrecipes.backend;
 
+import de.worldofrecipes.backend.model.Recipe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RecipeService {
+
+    private RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+
+    }
+
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.getAllRecipes();
+    }
 }
