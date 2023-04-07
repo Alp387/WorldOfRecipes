bd40aa92c0fa5ca6fc049657c1b699030da540af
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-04-08 00:15:09 +0200

    Recipes, RecipeController, RecipeService, RecipeRepository added with annotations

---
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeController.java
@@ -0,0 +1,11 @@
+package de.worldofrecipes.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/recipes")
+public class RecipeController {
+}
---
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeService.java
@@ -0,0 +1,9 @@
+package de.worldofrecipes.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecipeService {
+}
