18de4b36c38b76d99799ae3563812f7da8ad8479
Hasan A. Gürbüz <alperen87@hotmail.com>
2023-04-08 01:06:32 +0200

    BackendApplication response with 3 Recipes at GET-Endpoint "/api/recipes"

--- backend/src/main/java/de/worldofrecipes/backend/RecipeController.java
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeController.java
@@ -1,24 +1,22 @@
 package de.worldofrecipes.backend;
 
 import de.worldofrecipes.backend.model.Recipe;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/recipes")
 public class RecipeController {
-    private RecipeService recipeService;
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("")
+    @GetMapping()
     public List<Recipe> getAllRecipes() {
         return recipeService.getAllRecipes();
     }
--- backend/src/main/java/de/worldofrecipes/backend/RecipeService.java
+++ backend/src/main/java/de/worldofrecipes/backend/RecipeService.java
@@ -1,20 +1,17 @@
 package de.worldofrecipes.backend;
 
 import de.worldofrecipes.backend.model.Recipe;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class RecipeService {
 
     private RecipeRepository recipeRepository;
 
     public RecipeService(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
-
     }
 
     public List<Recipe> getAllRecipes() {
